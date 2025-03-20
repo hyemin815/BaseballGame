98f049835e4e0068f62f6fff0b0ab2302c1f4041
hyemin <hmpark15@gmail.com>
2025-03-21 00:42:29 +0900

    Refactor: generator for three random numbers
    modified generator

--- BaseballGame/main.swift
+++ BaseballGame/main.swift
@@ -1,11 +1,23 @@
-//
-//  main.swift
-//  BaseballGame
-//
-//  Created by 박혜민 on 3/19/25.
-//
-
 import Foundation
 
-print("Hello, World!")
-
+class BaseballGame {
+    func start() {
+        let answer = makeAnswer()
+        print("< 게임을 시작합니다 >")
+    }
+    
+    func makeAnswer() -> Int {
+        
+        while true {
+            var randomAnswer = Int.random(in: 100...999)
+            
+            let a = randomAnswer % 10
+            let b = (randomAnswer % 100 - a) / 10
+            let c = (randomAnswer - (b * 10) - a ) / 100
+            
+            if (a != b) && (a != c) && (b != c) && a != 0 && b != 0 && c != 0 {
+                return randomAnswer
+            }
+        }
+    }
+}
